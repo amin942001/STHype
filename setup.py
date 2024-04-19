4e125224ef2dfe2df4dc261b0944c23c3c8bc1d9
Amin Hamzaoui <amin.hamzaoui@etu.minesparis.psl.eu>
2024-04-19 17:18:23 +0200

    setup update

--- setup.py
+++ setup.py
@@ -1,19 +1,18 @@
 from setuptools import find_packages, setup
 
-# with open("STHype/README.md", "r") as f:
-#     long_description = f.read()
+with open("sthype/README.md", "r") as f:
+    long_description = f.read()
 
 setup(
     name="sthype",
     version="0.0.1",
-    description="Generate hypergrapsh from time correlated spatial graphs",
-    # package_dir={"": "STHype"},
-    packages=find_packages(where="."),
-    # long_description=long_description,
-    # long_description_content_type="text/markdown",
+    description="Generate hypergraph from time correlated spatial graphs",
+    packages=find_packages(where="sthype"),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/amin942001/STHype",
     license="MIT",
-    install_requires=[""],
+    install_requires=["networkx"],
     extras_require={
         "dev": [],
     },
---
+++ sthype/README.md
@@ -0,0 +1,5 @@
+# Classes
+
+## SpatialGraph:
+
+Essentially a nx.Graph with attribute _position_ for nodes and _pixels_ for edges
