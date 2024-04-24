7a0706e477e04431b0884e7891e6edee4df081ee
Amin Hamzaoui <amin.hamzaoui@etu.minesparis.psl.eu>
2024-04-24 10:25:40 +0200

    adjust boundaries for plotting

--- sthype/plot/plot_spatial_graph.py
+++ sthype/plot/plot_spatial_graph.py
@@ -27,8 +27,12 @@ def plot_spatial_graph(spatial_graph: SpatialGraph, plot=True) -> Figure:
         [edge_data["pixels"] for _, _, edge_data in spatial_graph.edges(data=True)]
     )
     fig, ax = plt.subplots()
-    ax.set_xlim(np.min(lines[:, :, 0]), np.max(lines[:, :, 0]))
-    ax.set_ylim(np.min(lines[:, :, 1]), np.max(lines[:, :, 1]))
+    x_min, x_max = np.min(lines[:, :, 0]), np.max(lines[:, :, 0])
+    x_min, x_max = x_min - 0.1 * (x_max - x_min), x_max + 0.1 * (x_max - x_min)
+    y_min, y_max = np.min(lines[:, :, 1]), np.max(lines[:, :, 1])
+    y_min, y_max = y_min - 0.1 * (y_max - y_min), y_max + 0.1 * (y_max - y_min)
+    ax.set_xlim(x_min, x_max)
+    ax.set_ylim(y_min, y_max)
     line_segments = LineCollection(lines)  # type: ignore
     ax.add_collection(line_segments)
     if plot:
