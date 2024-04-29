55f1f6ec032f8db187aae63152ec7bc52c93a4d4
Amin Hamzaoui <amin.hamzaoui@yahoo.fr>
2024-04-29 16:20:59 +0200

    fix init import errors

--- sthype/__init__.py
+++ sthype/__init__.py
@@ -1,3 +1,5 @@
 """Init to make sthype import easier"""
 
 from .classes.spatial_graph import SpatialGraph
+
+__all__ = ("SpatialGraph",)
--- sthype/plot/__init__.py
+++ sthype/plot/__init__.py
@@ -1,3 +1,5 @@
 """Init to make sthype import easier"""
 
 from .plot_spatial_graph import plot_spatial_graph
+
+__all__ = ("plot_spatial_graph",)
