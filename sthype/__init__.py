21635a7da750ba3f7daf469d41f3ac6661780738
Amin Hamzaoui <amin.hamzaoui@yahoo.fr>
2024-04-30 11:06:26 +0200

    add hypergraph class in init

--- sthype/__init__.py
+++ sthype/__init__.py
@@ -1,5 +1,6 @@
 """Init to make sthype import easier"""
 
+from .classes.hypergraph import HyperGraph
 from .classes.spatial_graph import SpatialGraph
 
-__all__ = ("SpatialGraph",)
+__all__ = ("SpatialGraph", "HyperGraph")
