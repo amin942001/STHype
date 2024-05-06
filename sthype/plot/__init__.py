c93ff043146f58c403c5cd77164988f9a833421a
Amin Hamzaoui <amin.hamzaoui@yahoo.fr>
2024-05-06 12:18:43 +0200

    add plot_spatial_graph_node to init

--- sthype/plot/__init__.py
+++ sthype/plot/__init__.py
@@ -1,3 +1,3 @@
-from .plot_spatial_graph import plot_spatial_graph
+from .plot_spatial_graph import plot_spatial_graph, plot_spatial_graph_node
 
-__all__ = ("plot_spatial_graph",)
+__all__ = ("plot_spatial_graph", "plot_spatial_graph_node")
